34030377ed33dc05c8809609f2e964a72af81f58
parangeomkar <zero.omkar@gmail.com>
2022-10-28 16:11:16 +0530

    Adds matrix addition logic with test cases

--- modules/math.js
+++ modules/math.js
@@ -1,10 +1,17 @@
 const Matrix = require("./matrix");
 
+/**
+ * 
+ * @param {*} A Matrix 1 of any size
+ * @param {*} B Matrix 2 of any size
+ * @returns returns dot product of A and B
+ */
 const dot = (A, B) => {
     // temp matrix
     let _dotMat = new Matrix();
     let isVectorProduct = 0;
 
+    // check if any matrix is null
     if (A.MAT.dim[0] >= 1 && A.MAT.dim[1] >= 1 && B.MAT.dim[0] >= 1 && B.MAT.dim[1] >= 1) {
 
         if (A.MAT.dim[0] == 1 && A.MAT.dim[1] == 1) {
@@ -60,6 +67,64 @@ const dot = (A, B) => {
     return _dotMat;
 }
 
+
+
+/**
+ * 
+ * @param {*} A Matrix 1 of any size
+ * @param {*} B Matrix 2 of any size
+ * @returns returns sum of A and B
+ */
+const add = (A, B) => {
+    // temp matrix
+    let _sumMat = new Matrix();
+    let isVectorSum = 0;
+
+    // check if any matrix is null
+    if (A.MAT.dim[0] >= 1 && A.MAT.dim[1] >= 1 && B.MAT.dim[0] >= 1 && B.MAT.dim[1] >= 1) {
+
+        if (A.MAT.dim[0] == 1 && A.MAT.dim[1] == 1) {
+            // initialize zero matrix of A->rows and A->columns
+            _sumMat.zeros(B.MAT.dim[0], B.MAT.dim[1]);
+
+        } else if (B.MAT.dim[0] == 1 && B.MAT.dim[1] == 1) {
+            // initialize zero matrix of B->rows and B->columns
+            _sumMat.zeros(A.MAT.dim[0], A.MAT.dim[1]);
+
+            // swap A and B to allow sequence of scaler param first in multiplication
+            let temp = B;
+            B = A;
+            A = temp;
+
+        } else {
+            // vector product
+            isVectorSum = 1;
+
+            // initialize zero matrix of A->rows and B->columns
+            _sumMat.zeros(A.MAT.dim[0], B.MAT.dim[1]);
+        }
+    } else {
+        throw new Error("Dot product with null matrix is attempted");
+    }
+
+    // iterate rows of A and multiply with columns of B
+    for (let i = 0; i < B.MAT.dim[0]; i++) {
+        for (let j = 0; j < B.MAT.dim[1]; j++) {
+            if (isVectorSum) {
+                _sumMat.MAT.val[i][j] = A.MAT.val[i][j] + B.MAT.val[i][j];
+            } else {
+                _sumMat.MAT.val[i][j] = A.MAT.val[0][0] + B.MAT.val[i][j];
+            }
+
+            // sum is NaN if dimensions do not match
+            if (isNaN(_sumMat.MAT.val[i][j])) {
+                throw new Error("Matrix dimensions do not match!");
+            }
+        }
+    }
+    return _sumMat;
+}
 module.exports = {
-    dot
+    dot,
+    add
 }
--- modules/math.test.js
+++ modules/math.test.js
@@ -1,4 +1,4 @@
-const { dot } = require("./math.js");
+const { dot, add } = require("./math.js");
 const Matrix = require("./matrix.js");
 
 test("Tests dot product of 1x1 and 1x1 arrays", () => {
@@ -96,8 +96,120 @@ test("Tests dot product of 2x2 and 2x2 arrays", () => {
 
 test("Tests dot product of 3x3 and 3x2 arrays", () => {
     let A = new Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]]);
-    let B = new Matrix([[5, 6], [7, 8], [12, 3]]);    
+    let B = new Matrix([[5, 6], [7, 8], [12, 3]]);
     let C = new Matrix([[55, 31], [127, 82], [199, 133]]);
 
     expect(dot(A, B)).toEqual(C);
+});
+
+
+
+
+
+
+
+
+
+///////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+test("Tests addition of 1x1 and 1x1 arrays", () => {
+    let A = new Matrix(4);
+    let B = new Matrix(7);
+    let C = new Matrix(11);
+
+    expect(add(A, B)).toEqual(C);
+});
+
+test("Tests addition of 1x1 and 1x2 arrays", () => {
+    let A = new Matrix(2);
+    let B = new Matrix([5, 6]);
+    let C = new Matrix([[7, 8]]);
+
+    expect(add(A, B)).toEqual(C);
+});
+
+test("Tests addition of 1x1 and 2x1 arrays", () => {
+    let A = new Matrix(4);
+    let B = new Matrix([[5], [6]]);
+    let C = new Matrix([[9], [10]]);
+
+    expect(add(A, B)).toEqual(C);
+});
+
+test("Tests addition of 1x1 and 2x2 arrays", () => {
+    let A = new Matrix(4);
+    let B = new Matrix([[5, 6], [7, 8]]);
+    let C = new Matrix([[9, 10], [11, 12]]);
+
+    expect(add(A, B)).toEqual(C);
+});
+
+
+test("Tests addition of 1x2 and 1x1 arrays", () => {
+    let A = new Matrix([5, 6]);
+    let B = new Matrix(2);
+    let C = new Matrix([[7, 8]]);
+
+    expect(add(A, B)).toEqual(C);
+});
+
+test("Tests addition of 2x1 and 1x1 arrays", () => {
+    let A = new Matrix([[5], [6]]);
+    let B = new Matrix(4);
+    let C = new Matrix([[9], [10]]);
+
+    expect(add(A, B)).toEqual(C);
+});
+
+test("Tests addition of 2x2 and 1x1 arrays", () => {
+    let A = new Matrix([[5, 6], [7, 8]]);
+    let B = new Matrix(4);
+    let C = new Matrix([[9, 10], [11, 12]]);
+
+    expect(add(A, B)).toEqual(C);
+});
+
+
+test("Tests addition of 1x2 and 1x2 arrays", () => {
+    let A = new Matrix([[1, 2]]);
+    let B = new Matrix([5, 6]);
+    let C = new Matrix([[6, 8]]);
+
+    expect(add(A, B)).toEqual(C);
+});
+
+test("Tests addition of 2x1 and 2x1 arrays", () => {
+    let A = new Matrix([[1], [2]]);
+    let B = new Matrix([[5], [6]]);
+    let C = new Matrix([[6], [8]]);
+
+    expect(add(A, B)).toEqual(C);
+});
+
+test("Tests addition of 2x2 and 2x2 arrays", () => {
+    let A = new Matrix([[1, 2], [3, 4]]);
+    let B = new Matrix([[5, 6], [7, 8]]);
+    let C = new Matrix([[6, 8], [10, 12]]);
+
+    expect(add(A, B)).toEqual(C);
+});
+
+
+test("Tests addition of 1x3 and 1x2 arrays", () => {
+    let A = new Matrix([[1, 2, 3]]);
+    let B = new Matrix([[5, 6, 7]]);
+    let C = new Matrix([6, 8, 10]);
+
+    expect(add(A, B)).toEqual(C);
 });
