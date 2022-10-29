6c7f081ed01b9d37ed0b6bcf1e5a44ee024304c0
parangeomkar <zero.omkar@gmail.com>
2022-10-29 11:47:43 +0530

    Updates matrix implementation and test cases

--- modules/matrix.js
+++ modules/matrix.js
@@ -1,69 +1,85 @@
 class Matrix {
     constructor(X) {
-        this.MAT = this.toMatrix(X);
+        let _mat = this.toMatrix(X);
+
+        this.val = _mat.val;
+        this.dim = _mat.dim;
     }
 
-    transpose() {
-        let _mat = this.MAT;
-        let x = this.MAT.val;
-        let tempMat = [[]];
+    /**
+     * 
+     * @param {*} i row number - starts from 0
+     * @param {*} j col number - starts from 0
+     * @returns matrix element at i'th row and j'th column
+     */
+    get(i, j) {
+        return this.val[i][j];
+    }
 
-        if (Array.isArray(x[0])) {
-            for (let i = 0; i < x.length; i++) {
-                let y = x[i];
-                for (let j = 0; j < y.length; j++) {
-                    if (!tempMat[j]) {
-                        tempMat[j] = [];
-                    }
-                    tempMat[j][i] = y[j];
+    /**
+     * transposes MAT property
+     */
+    transpose() {
+        let _mat = [[]];
+
+        for (let i = 0; i < this.val.length; i++) {
+            let y = this.val[i];
+            for (let j = 0; j < y.length; j++) {
+                if (!_mat[j]) {
+                    _mat[j] = [];
                 }
+                _mat[j][i] = y[j];
             }
         }
 
-        _mat.val = tempMat;
-        _mat.dim = [_mat.dim[1], _mat.dim[0]];
-
-        this.MAT = _mat;
+        this.val = _mat;
+        this.dim = [this.dim[1], this.dim[0]];
     }
 
+    /**
+     * 
+     * @param {*} rows number of rows
+     * @param {*} cols number of columns
+     * @brief sets MAT property 
+     */
     identity(rows, cols) {
-        this.MAT.val = new Array(rows).fill(1).map(() => new Array(cols).fill(1));
-        this.MAT.dim = [rows, cols];
+        this.val = new Array(rows).fill(1).map(() => new Array(cols).fill(1));
+        this.dim = [rows, cols];
     }
 
     zeros(rows, cols) {
-        this.MAT.val = new Array(rows).fill(0).map(() => new Array(cols).fill(0));
-        this.MAT.dim = [rows, cols];
+        this.val = new Array(rows).fill(0).map(() => new Array(cols).fill(0));
+        this.dim = [rows, cols];
     }
 
     toMatrix(X) {
-        let mat = {}
+        let _mat = {}
         if (X != undefined || !isNaN(X)) {
             if (Array.isArray(X)) {
                 if (!this.validateDim(X)) {
-                    throw new Error("Number of columns do not match for all rows!");
+                    throwError("Number of columns do not match for all rows!");
                 }
                 if (Array.isArray(X[0])) {
                     let rows = X.length;
                     let cols = X[0].length;
 
-                    mat.val = X;
-                    mat.dim = [rows, cols];
+                    _mat.val = X;
+                    _mat.dim = [rows, cols];
                 } else {
                     let cols = X.length;
 
-                    mat.val = [X];
-                    mat.dim = [1, cols];
+                    _mat.val = [X];
+                    _mat.dim = [1, cols];
                 }
             } else {
-                mat.val = [[X]];
-                mat.dim = [1, 1];
+                _mat.val = [[X]];
+                _mat.dim = [1, 1];
             }
         } else {
-            mat.val = [[]];
-            mat.dim = [0, 0];
+            _mat.val = [[]];
+            _mat.dim = [0, 0];
         }
-        return mat;
+        return _mat;
     }
 
     validateDim(X) {
--- modules/matrix.test.js
+++ modules/matrix.test.js
@@ -4,31 +4,31 @@ test("Tests empty matrix creation", () => {
     let A = new Matrix();
     let B = { val: [[]], dim: [0, 0] };
 
-    expect(A.MAT).toEqual(B);
+    expect({ val: A.val, dim: A.dim }).toEqual(B);
 });
 
 test("Tests 1x1 matrix creation", () => {
     let A = new Matrix(12);
     let B = { val: [[12]], dim: [1, 1] };
 
-    expect(A.MAT).toEqual(B);
+    expect({ val: A.val, dim: A.dim }).toEqual(B);
 });
 
 
 test("Tests 1x3 matrix transposition", () => {
     let A = new Matrix([1, 2, 3]);
-    let B = { val: [[1], [2], [3]], dim: [3, 1] };
+    let B = new Matrix([[1], [2], [3]]);
     A.transpose();
 
-    expect(A.MAT).toEqual(B);
+    expect(A).toEqual(B);
 });
 
 test("Tests 2x2 matrix transposition", () => {
     let A = new Matrix([[1, 2], [3, 4]]);
-    let B = { val: [[1, 3], [2, 4]], dim: [2, 2] };
+    let B = new Matrix([[1, 3], [2, 4]]);
     A.transpose();
 
-    expect(A.MAT).toEqual(B);
+    expect(A).toEqual(B);
 });
 
 test("Tests creation of 2x3 identity matrix", () => {
@@ -36,7 +36,7 @@ test("Tests creation of 2x3 identity matrix", () => {
     let B = { val: [[1, 1, 1], [1, 1, 1]], dim: [2, 3] };
     A.identity(2, 3);
 
-    expect(A.MAT).toEqual(B);
+    expect(A).toEqual(B);
 });
 
 
@@ -45,5 +45,5 @@ test("Tests creation of 3x2 zero matrix", () => {
     let B = { val: [[0, 0], [0, 0], [0, 0]], dim: [3, 2] };
     A.zeros(3, 2);
 
-    expect(A.MAT).toEqual(B);
+    expect(A).toEqual(B);
 });
